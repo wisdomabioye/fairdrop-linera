8dd1fdaef01fa43ee86af5571fa06a999b1cfed8
wisdomabioye <wisdomabioye@gmail.com>
2026-01-10 11:29:45 +0100

    feat: Update token definitions and supported tokens for auction payments

--- frontend/config/app.token-store.ts
+++ frontend/config/app.token-store.ts
@@ -4,26 +4,58 @@ export type TokenInfo = {
     name: string;
     symbol: string;
     appId: string;
-    id: number
 }
 
 
-const LUSD_APP_ID = '90c35bf5f9f580bfe75c38f3fd6ec07e2386d43a3e561d9b9a3b47eabc96be2d';
-const FUSD_APP_ID = 'b025bec560dffb150616b687b0aff00c94dd46f4448a5929e3c6d4b35712e386';
+const LUSD_APP_ID = '6f0b5c1c79c7a2f1f0bbd1ae1407788cbf02fcbe3c0d2f589f73d593bc3d31af';
+const FUSD_APP_ID = '413b030e15c3ae91fe8379d5ad3fef7e30e5de36bfb6b4bf33febd2dceac84a7';
+const XUSD_APP_ID = '07e95721ac98bca199d8fae91bb4cb19246195349a71ed9d86215996148b90aa';
+const SPL_APP_ID = 'b1d4b85ce5d63e97a3134111aaf9be9e5e0e210078ae8e0395c8f3612b4a91ce';
+const DIGI_APP_ID = 'c433e8e86d836f18d28eb111688b0f501ae3a32b9b07be11b07ffa041d4677c5';
+const FAIR_APP_ID = 'a1178930ef5bf4d77cf8f34028b153575534b02dc9340367562333c566671523';
 
-export const TOKEN_STORE: Record<ApplicationId, TokenInfo> = {
+/** List of supported tokens for auction payment */
+export const PAYMENT_TOKEN: Record<ApplicationId, TokenInfo> = {
     [LUSD_APP_ID]: {
         name: 'LN USD Token',
         symbol: 'LUSD',
         appId: LUSD_APP_ID,
-        id: 0,
     },
     [FUSD_APP_ID]: {
         name: 'fUSD Token',
         symbol: 'fUSD',
         appId: FUSD_APP_ID,
-        id: 1
+    },
+    [XUSD_APP_ID]: {
+        name: 'xUSD Token',
+        symbol: 'xUSD',
+        appId: XUSD_APP_ID,
     }
+} as const
+
+/** List of token that can be auctioned (for development purpose) */
+export const AUCTION_TOKEN: Record<ApplicationId, TokenInfo> = {
+    [SPL_APP_ID]: {
+        name: 'SPleen Token',
+        symbol: 'SPL',
+        appId: SPL_APP_ID,
+    },
+    [DIGI_APP_ID]: {
+        name: 'DIGI Token',
+        symbol: 'DIGI',
+        appId: DIGI_APP_ID,
+    },
+    [FAIR_APP_ID]: {
+        name: 'Fair Token',
+        symbol: 'FAIR',
+        appId: FAIR_APP_ID,
+    }
+} as const
+
+
+export const TOKEN_STORE: Record<ApplicationId, TokenInfo> = {
+    ...PAYMENT_TOKEN,
+    ...AUCTION_TOKEN
 }
 
 export const getTokenList = (): TokenInfo[] => {
@@ -31,7 +63,7 @@ export const getTokenList = (): TokenInfo[] => {
 }
 
 export const getTokenByIndex = (index: number): TokenInfo | undefined => {
-    return Object.values(TOKEN_STORE).find(token => token.id === index);
+    return Object.values(TOKEN_STORE)[index];
 }
 
 export const getTokenByAppId = (appId: string): TokenInfo => {
