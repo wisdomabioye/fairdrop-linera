ee4c49f9cb046d955b9c231eb2a6a4779379eef0
wisdomabioye <wisdomabioye@gmail.com>
2026-01-11 08:26:04 +0100

    refactor: Simplify required application IDs retrieval in deploy script

--- deploy-auction.sh
+++ deploy-auction.sh
@@ -3,16 +3,15 @@ set -eu
 
 JSON_FILE="json-parameter.json"
 
-REQUIRED_ARGS=""
-
-# Read tokens line-by-line safely
-jq -r '.supported_tokens[]' "$JSON_FILE" | while IFS= read -r token; do
-  REQUIRED_ARGS="$REQUIRED_ARGS --required-application-ids $token"
-done
+REQUIRED_IDS=$(
+  jq -r '.supported_tokens[]' "$JSON_FILE" \
+  | sed 's/^/--required-application-ids /' \
+  | tr '\n' ' '
+)
 
 # shellcheck disable=SC2086
 linera publish-and-create \
   target/wasm32-unknown-unknown/release/auction-contract.wasm \
   target/wasm32-unknown-unknown/release/auction-service.wasm \
   --json-parameters-path "$JSON_FILE" \
-  $REQUIRED_ARGS
+  $REQUIRED_IDS
